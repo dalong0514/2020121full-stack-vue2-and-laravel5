857e84fa69503e16bb814d4a6323c133fc1d401e
dalong <dalong0514@126.com>
2020-04-24 16:50:58 +0800

    0614-refactorying components to sfcs

--- laravel/resources/assets/js/app.js
+++ laravel/resources/assets/js/app.js
@@ -2,59 +2,10 @@
 import Vue from 'vue';
 // window.Vue = require('vue');
 import { populateAmenitiesAndPrices } from './helpers';
+import ImageCarousel from '../components/ImageCarousel.vue';
 
 let model = JSON.parse(window.vuebnb_listing_model);
 model = populateAmenitiesAndPrices(model);
-
-// images carousel
-Vue.component('image-carousel', {
-    template: `<div class="image-carousel">
-                    <img v-bind:src="image">
-                    <div class="controls">
-                        <carousel-control dir="left" @change-image="changeImage"></carousel-control>
-                        <carousel-control dir="right" @change-image="changeImage"></carousel-control>
-                    </div>
-               </div>`,
-    props: ['images'],
-    data() {
-        return {
-            index: 0,
-        }
-    },
-    computed: {
-        image() {
-            return this.images[this.index];
-        }
-    },
-    components: {
-        'carousel-control': {
-            template: `<i :class="classes" @click="clicked"></i>`,
-            props: ['dir'],
-            computed: {
-                classes() {
-                    return 'carousel-control fa fa-2x fa-chevron-' + this.dir;
-                }
-            },
-            methods: {
-                clicked() {
-                    this.$emit('change-image', this.dir === 'left' ? -1 : 1);
-                }
-            },
-        }
-    },
-    methods: {
-        changeImage(val) {
-            let newVal = this.index + parseInt(val);
-            if (newVal < 0) {
-                this.index = this.images.length - 1;
-            } else if (newVal === this.images.length) {
-                this.index = 0;
-            } else {
-                this.index = newVal;
-            }
-        },
-    },
-});
 
 // Vue 实例
 var vm = new Vue({
@@ -74,6 +25,7 @@ var vm = new Vue({
             }
         }
     },
+    components: { ImageCarousel },
     watch: {
         modalOpen: function() {
             var className = "modal-open";
@@ -91,20 +43,3 @@ var vm = new Vue({
         document.removeEventListener('keyup', this.escapeKeyListenser);
     },
 });
-
-// console.log(vm);
-
-// var vm1 = new Vue({
-//     el: "#app1",
-//     data: {
-//         val: 1,
-//     },
-//     computed: {
-//         message() {
-//             return `the value is ${this.val}`;
-//         },
-//     },
-// });
-// setTimeout(function() {
-//     vm.val = 2;
-// }, 2000);
