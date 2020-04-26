e7f64c52fa6d2fcb93a95070168ee5c5cb09d694
dalong <dalong0514@126.com>
2020-04-26 10:11:24 +0800

    0620-start

---
+++ laravel/resources/assets/components/FeatureList.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <hr>
+        <div class="list">
+            <div class="title"><strong>{{ title }}</strong></div>
+            <div class="content">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
--- laravel/resources/views/test.blade.php
+++ laravel/resources/views/test.blade.php
@@ -8,57 +8,63 @@
 <body>
     <div id="app">
         <my-component @toggle="toggle=!toggle"></my-component>
-        @{{ message }}
+        @{{ comtest }}
+        <br>
+        <input type="radio" v-model="pick" :value="a">
     </div>
 
     <div id="app1">
-        <ol>
-        <todo-item 
-            v-for="item in grocerylist"
-            :todo="item"
-            :key="item.id"></todo-item>
-        </ol>
+        <input v-model="message" placeholder="edit me">
+        <p>message is: @{{ message }}</p>
+        <span>multline message is:</span>
+        <br>
+        <textarea v-model="message" placeholder="edit me"></textarea>
+        <p style="white-space: pre-line;">@{{ message }}</p>
     </div>
-    <!-- <script src="{{ asset('js/testapp.js') }}" type="text/javascript"></script> -->
-    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-    <script>
-        Vue.component('my-component', {
-            template: '<div v-on:click="clicked">click me</div>',
-            methods: {
-                clicked: function() {
-                    this.$emit('toggle');
-                },
-            },
-        });
-
-        var vm = new Vue({
-            el: "#app",
-            data: {
-                toggle: false,
-            },
-            computed: {
-                message: function() {
-                    return this.toggle ? 'On' : 'Off';
-                },
-            },
-        });
-
-        Vue.component('todo-item', {
-            props: ['todo'],
-            template: '<li>@{{ todo.text }}</li>',
-        });
-
-        var vm1 = new Vue({
-            el: "#app1",
-            data: {
-                grocerylist: [
-                    {id:0, text:'蔬菜'},
-                    {id:1, text:'蔬菜1'},
-                    {id:2, text:'蔬菜2'},
-                    {id:3, text:'蔬菜3'},
-                ]
-            },
-        });
-    </script>
 </body>
+
+<!-- <script src="{{ asset('js/testapp.js') }}" type="text/javascript"></script> -->
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+<script>
+    Vue.component('my-component', {
+        template: '<div v-on:click="clicked">click me</div>',
+        methods: {
+            clicked: function() {
+                this.$emit('toggle');
+            },
+        },
+    });
+
+    var vm = new Vue({
+        el: "#app",
+        data: {
+            toggle: false,
+            pick: "",
+            a: "dalong",
+        },
+        computed: {
+            comtest: function() {
+                return this.toggle ? 'On' : 'Off';
+            },
+        },
+    });
+
+    Vue.component('todo-item', {
+        props: ['todo'],
+        template: '<li>@{{ todo.text }}</li>',
+    });
+
+    var vm1 = new Vue({
+        el: "#app1",
+        data: {
+            grocerylist: [
+                {id:0, text:'蔬菜'},
+                {id:1, text:'蔬菜1'},
+                {id:2, text:'蔬菜2'},
+                {id:3, text:'蔬菜3'},
+            ],
+            message: "",
+        },
+    });
+</script>
 </html>
