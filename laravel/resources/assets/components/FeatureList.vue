5f4910b3fa47b46a7dff32afb55f94ce3c5c8ba2
dalong <dalong0514@126.com>
2020-04-26 10:56:49 +0800

    0621-Scoped slots

--- laravel/resources/assets/components/FeatureList.vue
+++ laravel/resources/assets/components/FeatureList.vue
@@ -4,60 +4,62 @@
         <div class="list">
             <div class="title"><strong>{{ title }}</strong></div>
             <div class="content">
-                <slot></slot>
+                <div class="list-item" v-for="item in items">
+                    <slot v-bind="item"></slot>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-export default {
-    props: ['title'],
-}
+    export default {
+        props: ['title', 'items'],
+    }
 </script>
 
 <style>
-hr {
-    border: 0;
-    border-top: 1px solid #dce0e0;
-}
+    hr {
+        border: 0;
+        border-top: 1px solid #dce0e0;
+    }
 
-.list {
-    display: flex;
-    flex-wrap: nowrap;
-    margin: 2em 0;
-}
+    .list {
+        display: flex;
+        flex-wrap: nowrap;
+        margin: 2em 0;
+    }
 
-.list .title {
-    flex: 1 1 25%;
-  }
-  
-.list .content {
-    flex: 1 1 75%;
-    display: flex;
-    flex-wrap: wrap;
-}
-  
-.list .list-item {
-    flex: 0 0 50%;
-    margin-bottom: 16px;
-}
-  
-.list .list-item > i {
-    width: 35px;
-}
-
-@media (max-width: 743px) {
     .list .title {
-      flex: 1 1 33%;
+        flex: 1 1 25%;
     }
-  
+    
     .list .content {
-      flex: 1 1 67%;
+        flex: 1 1 75%;
+        display: flex;
+        flex-wrap: wrap;
     }
-  
+    
     .list .list-item {
-      flex: 0 0 100%;
+        flex: 0 0 50%;
+        margin-bottom: 16px;
     }
-}
+    
+    .list .list-item > i {
+        width: 35px;
+    }
+
+    @media (max-width: 743px) {
+        .list .title {
+        flex: 1 1 33%;
+        }
+    
+        .list .content {
+        flex: 1 1 67%;
+        }
+    
+        .list .list-item {
+        flex: 0 0 100%;
+        }
+    }
 </style>
