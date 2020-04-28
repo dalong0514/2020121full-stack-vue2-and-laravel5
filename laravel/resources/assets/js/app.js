d8b10dd5e7982f1bd301e909380fafdeaaa59af4
dalong <dalong0514@126.com>
2020-04-28 21:02:10 +0800

    0802-install vuex

--- laravel/resources/assets/js/app.js
+++ laravel/resources/assets/js/app.js
@@ -4,6 +4,7 @@ import Vue from 'vue';
 import ListingPage from '../components/ListingPage.vue';
 import App from '../components/App.vue';
 import router from './router';
+import store from './store';
 
 // Vue 实例
 var vm = new Vue({
@@ -11,4 +12,5 @@ var vm = new Vue({
     render: h => h(App),
     // components: {ListingPage},
     router,
+    store,
 });
